d077c24739c1712cd69f5f6c37b95b8a32920969
Natalya Krukova <k-natali2009@mail.ru>
2026-04-07 02:56:17 +0300

    fixing subclass LawnGrass

--- src/heir_class.py
+++ src/heir_class.py
@@ -24,7 +24,7 @@ class LawnGrass (Product):
                  color: str):
         super().__init__(name, description, price, quantity)
         self.country = country
-        self.germination_period = germination_periodsupclass
+        self.germination_period = germination_period
         self.color = color
 
     def __add__(self, other):
